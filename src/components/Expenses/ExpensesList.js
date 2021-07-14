8b04790bf6c149cbdadbe4c2e888f1a1e0bb5a08
janae-b <janae.boersma@comcast.net>
2021-07-13 23:22:52 -0500

    add list div

--- src/components/Expenses/ExpensesList.js
+++ src/components/Expenses/ExpensesList.js
@@ -8,7 +8,8 @@ const ExpensesList = props => {
     return <h2 className="expenses-list__fallback">Found no expenses.</h2>
   }
 
-  return <ul className="expenses-list">
+  return (
+  <ul className="expenses-list">
     {props.items.map((expense) => (
       <ExpenseItem
         key={expense.id}
@@ -19,7 +20,8 @@ const ExpensesList = props => {
     ))};
 
   </ul>
-};
+  );
+}
 
 export default ExpensesList;
 
